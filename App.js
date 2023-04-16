f03bfeccf0d504811ef2c6c713cbc99831330c5b
Rafael Londoño <athenapms12345@gmail.com>
2023-04-15 19:28:24 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, TouchableOpacity, SafeAreaView, StatusBar } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Código QR escaneado: ${data}`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Solicitando permiso de la cámara</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No se otorgó acceso a la cámara</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Lector de Código QR</Text>
+      </View>
+      <View style={styles.scannerContainer}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </View>
+      {scanned && (
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => setScanned(false)}>
+            <Text style={styles.buttonText}>Volver a escanear</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+  },
+  header: {
+    backgroundColor: '#1c1c1c',
+    padding: 15,
+  },
+  headerText: {
+    fontSize: 18,
+    color: '#FFF',
+    textAlign: 'center',
+  },
+  scannerContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  buttonContainer: {
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  button: {
+    backgroundColor: '#1c1c1c',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#FFF',
   },
 });
